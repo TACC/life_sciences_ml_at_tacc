089b401da40ef455d9135ef751d0a01f45471c53
kbeavers <kbeavers@tacc.utexas.edu>
2025-03-28 16:51:57 -0500

    Add helper scripts for dataset download and kernel install

---
+++ docs/section3/lemon-leaf-cnn-tutorial/download_dataset.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+wget -O $SCRATCH/lemon-leaf-diseases.zip https://www.kaggle.com/api/v1/datasets/download/mahmoudshaheen1134/lemon-leaf-disease-dataset-lldd
+unzip $SCRATCHlemon-leaf-diseases.zip
