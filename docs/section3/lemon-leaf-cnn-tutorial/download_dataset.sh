7d0d2e193479cab1c1cf4c41fd6297671246df17
kbeavers <kbeavers@tacc.utexas.edu>
2025-04-09 14:21:44 -0500

    Add Hands-on-CNN training materials

--- docs/section3/lemon-leaf-cnn-tutorial/download_dataset.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-wget -O $SCRATCH/lemon-leaf-diseases.zip https://www.kaggle.com/api/v1/datasets/download/mahmoudshaheen1134/lemon-leaf-disease-dataset-lldd
-unzip $SCRATCHlemon-leaf-diseases.zip
